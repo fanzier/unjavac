ad351b24636688c0e968fc99b73d9f83395caa32
Fabian Zaiser <fabian.zaiser@gmail.com>
2017-03-03 17:05:27 +0100

    Extend parsing to complete a very simple class file

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,9 @@ fn parse_class_file<R: Read>(input: &mut R) -> Result<ClassFile> {
     let access_flags = AccessFlags::from_bits(input.read_u16::<BigEndian>()?).unwrap();
     let this_class = input.read_u16::<BigEndian>()?;
     let super_class = input.read_u16::<BigEndian>()?;
+    let interfaces = parse_interfaces(input)?;
+    let fields = parse_fields(input)?;
+    let methods = parse_methods(input)?;
     Ok(ClassFile {
         magic: magic,
         minor_version: minor_version,
@@ -29,6 +32,9 @@ fn parse_class_file<R: Read>(input: &mut R) -> Result<ClassFile> {
         access_flags: access_flags,
         this_class: this_class,
         super_class: super_class,
+        interfaces: interfaces,
+        fields: fields,
+        methods: methods,
     })
 }
 
@@ -79,6 +85,9 @@ struct ClassFile {
     access_flags: AccessFlags,
     this_class: u16,
     super_class: u16,
+    interfaces: Vec<u16>,
+    fields: Vec<FieldInfo>,
+    methods: Vec<MethodInfo>,
 }
 
 #[derive(Debug)]
@@ -94,6 +103,82 @@ enum ConstantPoolInfo {
         name_index: u16,
         descriptor_index: u16,
     },
+}
+
+fn parse_interfaces<R: Read>(input: &mut R) -> Result<Vec<u16>> {
+    let count = input.read_u16::<BigEndian>()?;
+    let mut interfaces = vec![];
+    for _ in 0..count {
+        unimplemented!()
+    }
+    Ok(interfaces)
+}
+
+fn parse_fields<R: Read>(input: &mut R) -> Result<Vec<FieldInfo>> {
+    let count = input.read_u16::<BigEndian>()?;
+    let mut fields = vec![];
+    for _ in 0..count {
+        unimplemented!()
+    }
+    Ok(fields)
+}
+
+#[derive(Debug)]
+struct FieldInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
+fn parse_methods<R: Read>(input: &mut R) -> Result<Vec<MethodInfo>> {
+    let count = input.read_u16::<BigEndian>()?;
+    let mut methods = vec![];
+    for _ in 0..count {
+        let access_flags = input.read_u16::<BigEndian>()?;
+        let name_index = input.read_u16::<BigEndian>()?;
+        let descriptor_index = input.read_u16::<BigEndian>()?;
+        let attributes = parse_attributes(input)?;
+        let method = MethodInfo {
+            access_flags: access_flags,
+            name_index: name_index,
+            descriptor_index: descriptor_index,
+            attributes: attributes,
+        };
+        methods.push(method);
+    }
+    Ok(methods)
+}
+
+#[derive(Debug)]
+struct MethodInfo {
+    access_flags: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    attributes: Vec<AttributeInfo>,
+}
+
+fn parse_attributes<R: Read>(input: &mut R) -> Result<Vec<AttributeInfo>> {
+    let count = input.read_u16::<BigEndian>()?;
+    let mut attributes = vec![];
+    for _ in 0..count {
+        let attribute_name_index = input.read_u16::<BigEndian>()?;
+        let attribute_length = input.read_u32::<BigEndian>()?;
+        let mut info = vec![0; attribute_length as usize];
+        input.read_exact(&mut info);
+        let attribute = AttributeInfo {
+            attribute_name_index: attribute_name_index,
+            info: info,
+        };
+        attributes.push(attribute);
+    }
+    Ok(attributes)
+}
+
+#[derive(Debug)]
+struct AttributeInfo {
+    attribute_name_index: u16,
+    info: Vec<u8>,
 }
 
 bitflags! {
